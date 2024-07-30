09c1a1b9a95b3265237b362fd684babf252c0b53
ASHIKEkfrazo <ashik@ekfrazo.in>
2024-07-30 15:33:15 +0530

    APi url Changes

--- src/API/API.js
+++ src/API/API.js
@@ -3,11 +3,13 @@ import axios from "axios";
 // const   baseURL =  'https://hul.aivolved.in/api/';
 // const baseURL =  'http://vin.aivolved.in:8100/';
 // const baseURL =  'http://159.65.157.118:8005/';
-const baseURL =  'http://159.65.157.118:8011/api/'
+// const baseURL =  'http://159.65.157.118:8011/api/'
 
 
 
 // const baseURL =  'http://localhost:8000/api/';
+const baseURL =  'http:localhost:8011/api/';
+
 
 const token = localStorage.getItem("token");
 const AuthToken = JSON.parse(token)
